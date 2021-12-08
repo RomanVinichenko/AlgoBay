c20c76c90161cfd80249f3078a1826f0163fc496
Roman Vinichenko <chicagezzz@gmail.com>
2021-12-09 01:25:12 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -49,3 +49,12 @@ const links = document.querySelector('.links');
 button.addEventListener('click', function () {
     links.classList.toggle('links__active');
 });
+
+const terms = document.querySelector('.terms__button-terms');
+const privacy = document.querySelector('.terms__button-privacy');
+privacy.addEventListener('click', function () {
+    privacy.classList.toggle('terms__button--active');
+});
+terms.addEventListener('click', function () {
+    terms.classList.toggle('terms__button--active');
+});
